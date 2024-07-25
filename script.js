cb9d1fdd55519b87b198fc7410db017768cadde5
SoraI0 <romamogir@gmail.com>
2024-07-25 23:20:51 +0300

    alpha, 0.0.2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const hui = document.querySelector('.hui')
-const cube = document.querySelector('.cube')
+const door = document.querySelector('.door')
 const body = document.querySelector('body')
 const gameField = document.querySelector('.game-field')
 const buttonStart = document.querySelector('.button-start')
@@ -12,10 +12,8 @@ huisCountOnScreen.style.transition = '0.2s'
 scoreCountOnScreen.style.transition = '0.2s'
 
 
-cube.style.width = '30px'
-cube.style.height = '30px'
-cube.style.left = '0px'
-cube.style.bottom = '0px'
+door.style.left = '0px'
+door.style.bottom = '0px'
 let isGame
 let huiSpawn
 let isJump = true
@@ -25,8 +23,10 @@ let huisCount = 0
 let score = 0
 let scoreCounter
 
+
 const endScreen = document.createElement('div')
     gameField.appendChild(endScreen)
+    endScreen.style.position = 'absolute'
     endScreen.style.width = '100%'
     endScreen.style.height = '100%'
     endScreen.style.display = 'none'
@@ -38,9 +38,20 @@ const endScreen = document.createElement('div')
     endScreen.style.backgroundColor = 'red'
     endScreen.style.transition = '0.5s'
 
+    const conv = document.createElement('img')
+    conv.setAttribute('src', './images/conv.PNG')
+    conv.classList.add('conv')
+    gameField.appendChild(conv)
 function gameStart() {
+
     reset()
     isGame = true
+    
+    // convMove = setInterval(()=>{
+    //     gameField.appendChild(conv)
+    //     conv.style.marginLeft = Number(conv.style.marginLeft.slice(0, -2)) - 1 + 'px'
+    // },5)
+
 
     huiSpawn = setInterval(()=>{
         setTimeout(()=>{
@@ -53,16 +64,20 @@ function gameStart() {
         scoreCountOnScreen.textContent = score
     },100)
     
-    cubeControl()
+    doorControl()
+}
+
+function convCreate() {
+    
 }
 
 function huiCreate() {
     const hui = document.createElement('div')
     hui.classList.add('hui')
-    hui.innerHTML = '<div class="hui__head"></div><div class="hui__stick"></div><div class="hui__balls"></div>'
+    hui.innerHTML = `<img src="./images/${getRndInteger(1, 6)}.png" alt="DICK :)">`
 
     
-    hui.style.width = '190px'
+    hui.style.width = '90px'
     hui.style.height = '90px'
     let huiPosition = 600
     hui.style.left = huiPosition + 'px'
@@ -78,28 +93,32 @@ function huiCreate() {
     function huiMove () {
     
         let huiMove = setInterval(()=>{
-            huiPosition = huiPosition - huiSpeed
+            // console.log('Hui: ',huiPosition)
+            // console.log('Door:',door.style.left)
+            if(huiPosition > -200) {huiPosition = huiPosition - huiSpeed}
+            
             hui.style.left = huiPosition + 'px'
-            if (collision(cube, hui)) {
-                clearInterval(huiMove)
-                hui.style.opacity = '20%'
-            }
-            if (huiPosition === -huiSpeed) {
+            
+            if (huiPosition === 0 ) {
+                
                 huisCount = huisCount + 1
                 huisCountOnScreen.textContent = huisCount
-                console.log('cheeeeck');
                 if(huisCount % 10 === 0 && huisCount >= -1){
                     huiSpeed = huiSpeed + 0.5
-                    console.log(true, huisCount, huiSpeed)
+                    console.log(true, huisCount, huiSpeed, huiPosition)
                 }
             }
             if (huiPosition === -200) {
                 hui.remove()
             }
-            if (isGame === false) {
+            if (collision(door, hui)) {
+                clearInterval(huiMove)
+                hui.style.opacity = '20%'
                 document.querySelectorAll('.hui').forEach(e => e.remove())
+                door.style.display = 'none'
             }
         }, 5)
+        
     }
     if(isGame) {
         huiMove()
@@ -121,9 +140,10 @@ function reset() {
     buttonRestart.style.display = 'none'
     scoreBoard.style.display =  'flex'
     gameField.style.marginTop = '0px'
+    door.style.display = 'block'
 
     endScreen.style.display = 'none'
-    cube.style.opacity = '100%'
+    door.style.opacity = '100%'
 }
 
 buttonStart.addEventListener('click', ()=> {
@@ -136,28 +156,23 @@ buttonRestart.addEventListener('click', ()=> {
 
 
 
-function collision (cube, hui) {
-    if (cube.style.left === hui.style.left && cube.style.bottom <= hui.style.height) {
-        console.log('collision')
-        
-        gameEnd()
-        return true
+function collision (door, hui) {
+    if (Number(door.style.left.slice(0, -2)) + 30 >= Number(hui.style.left.slice(0, -2)) && Number(door.style.left.slice(0, -2))+10 <= Number(hui.style.left.slice(0, -2)) && Number(door.style.bottom.slice(0, -2)) <= Number(hui.style.height.slice(0, -2))) {
+            console.log('collision')
+            gameEnd()
+            return true   
     }
 }
-let rotateDeg = 0
-function cubeControl () {
+function doorControl () {
         body.addEventListener('click', ()=>{
-            if (cube.style.bottom === '0px' && isGame) {
-                cube.style.transition = '0.5s'
-                rotateDeg = rotateDeg + 90
-                cube.style.rotate = rotateDeg + 'deg'
-                cube.style.bottom = '99px'
+            if (Number(door.style.bottom.slice(0, -2)) === 0 && isGame) {
+                door.style.transition = '0.5s'
+
+                door.style.bottom = '120px'
                 setTimeout(() => {
-                    cube.style.bottom = '1px'
-                    rotateDeg = rotateDeg + 90
-                    cube.style.rotate = rotateDeg + 'deg'
+                    door.style.bottom = '1px'
                     setTimeout(() => {
-                        cube.style.bottom = '0px'
+                        door.style.bottom = '0px'
                     }, 500);
                 }, 600);  
             }      
@@ -170,7 +185,7 @@ function gameEnd() {
     scoreBoard.style.display =  'none'
     gameField.style.marginTop = '33.33px'
     
-    cube.style.opacity = '20%'
+    door.style.opacity = '20%'
     
     buttonRestart.style.display = 'block'
     isGame = false
