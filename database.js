bdf81d804aeec4717c5286d9b5ef8f06f9b7bb3a
SoraI0 <romamogir@gmail.com>
2024-08-11 13:20:51 +0300

    beta 0.0.1

--- database.js
+++ database.js
@@ -2,85 +2,92 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHAcm-LxNf_GXREd8QlgnY3TWXEM17xMk",
-    authDomain: "dickfactory-df5d0.firebaseapp.com",
-    databaseURL: "https://dickfactory-df5d0-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "dickfactory-df5d0",
-    storageBucket: "dickfactory-df5d0.appspot.com",
-    messagingSenderId: "138074319344",
-    appId: "1:138074319344:web:4e0ca79424ef14db819535",
-    measurementId: "G-QXHRDP1JKH"
+	apiKey: "AIzaSyBHAcm-LxNf_GXREd8QlgnY3TWXEM17xMk",
+	authDomain: "dickfactory-df5d0.firebaseapp.com",
+	databaseURL: "https://dickfactory-df5d0-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "dickfactory-df5d0",
+	storageBucket: "dickfactory-df5d0.appspot.com",
+	messagingSenderId: "138074319344",
+	appId: "1:138074319344:web:4e0ca79424ef14db819535",
+	measurementId: "G-QXHRDP1JKH"
 };
 
 const app = initializeApp(firebaseConfig);
-import {getDatabase, ref, child, get, set, update, remove, onValue} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+import { getDatabase, ref, child, get, set, update, remove, onValue } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 const firebase = getDatabase();
 let userIP
 fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        userIP = data.ip
-    })
-    .catch(error => {
-        console.log('Error:', error);
-    });
+	.then(response => response.json())
+	.then(data => {
+		userIP = data.ip
+	})
+	.catch(error => {
+		console.log('Error:', error);
+	});
 
 export function saveUser(username) {
-    set(ref(firebase, 'Users/'+ username), {
-        huis: 0,
-        score: 0,
-        username: username,
-        ip: userIP
-    }).then(()=>{
-        console.log('Успішно збережено', username); 
-    })
+	set(ref(firebase, 'Users/' + username), {
+		huis: 0,
+		score: 0,
+		username: username,
+		ip: userIP
+	}).then(() => {
+		console.log('Успішно збережено', username);
+	})
 }
 
 export function saveScore(username, huis, score) {
-    set(ref(firebase, 'Users/'+ username), {
-        huis: huis,
-        score: score,
-        username: username,
-        ip: userIP
-    }).then(()=>{
-        console.log('Успішно збережено', huis, ' ', score); 
-    })
+	set(ref(firebase, 'Users/' + username), {
+		huis: huis,
+		score: score,
+		username: username,
+		ip: userIP
+	}).then(() => {
+		console.log('Успішно збережено', huis, ' ', score);
+	})
 }
 
 export let sortedUsers
 let usersArr
-export function getRecordBoard(board){
-    const db = getDatabase();
-    const UserstRef = ref(db, 'Users/');
-    
-    onValue(UserstRef, (snapshot) => {
-        board.innerHTML = ''
-        const data = snapshot.val();
-        usersArr = Object.values(data)
-        sortedUsers = usersArr.sort((u1, u2) => {
-            return  u2.score - u1.score;
-        })
+export function getRecordBoard(board) {
+	const db = getDatabase();
+	const UserstRef = ref(db, 'Users/');
 
-        let numScore = 1
-        sortedUsers.forEach(el=>{
-            el = Object.values(el)
-            let userRecord = document.createElement('div')
-            if (numScore <= 10) {
-                userRecord.innerHTML = `<div class="user-score"><span class="score-username">${numScore}. ${el[3]}: </span><span class="score-score"> ${el[2]}╭ᑎ╮${el[0]}</span></div>`
-            } else if (el[3] === localStorage.username) {
-                userRecord.innerHTML = `<div>...</div><div class="user-score"><span class="score-username">${numScore}. ${el[3]}: </span><span class="score-score"> ${el[2]}╭ᑎ╮${el[0]}</span></div>`
-            }
-            
-            board.appendChild(userRecord)
-            numScore++
+	onValue(UserstRef, (snapshot) => {
+		board.innerHTML = ''
+		const data = snapshot.val();
+		usersArr = Object.values(data)
+		sortedUsers = usersArr.sort((u1, u2) => {
+			return u2.score - u1.score;
+		})
 
-        
-        })
-        // console.log(usersArr);
-        
-        
-    })
+		let userScoreOutline = ''
+		let numScore = 1
+		sortedUsers.forEach(el => {
+			el = Object.values(el)
+			let userRecord = document.createElement('div')
+			if (numScore <= 10) {
+				if (el[3] === localStorage.username) { userScoreOutline = 'style="text-shadow: #fff 1px 0 10px;"' }
+				userRecord.innerHTML = `<div ${userScoreOutline} class="user-score"><span class="score-username">${numScore}. ${el[3]}: </span><span class="score-score"> ${el[2]}╭ᑎ╮${el[0]}</span></div>`
+				userScoreOutline = ''
+			} else if (el[3] === localStorage.username) {
+				userRecord.innerHTML = `<div>...</div><div class="user-score"><span class="score-username">${numScore}. ${el[3]}: </span><span class="score-score"> ${el[2]}╭ᑎ╮${el[0]}</span></div>`
+			}
+
+			if (el[3] === localStorage.username) {
+
+			}
+
+			board.appendChild(userRecord)
+			numScore++
+
+
+		})
+		// console.log(usersArr);
+
+
+	})
 }
 
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {saveScore, saveUser, getRecordBoard, sortedUsers} from "./database.js"
+import { saveScore, saveUser, getRecordBoard, sortedUsers } from "./database.js"
 
 const door = document.querySelector('.door')
 const body = document.querySelector('body')
@@ -48,7 +48,7 @@ door.style.left = '0px'
 door.style.bottom = '0px'
 let isGame
 let huiSpawn
-let huiSpeed = 1
+let convSpeed = 1
 
 let huisCount = 0
 let score = 0
@@ -67,15 +67,15 @@ convCreate(600)
 
 getRecordBoard(recordBoard)
 
-body.oncontextmenu = function(event) {
-    event.preventDefault();
-    event.stopPropagation(); // not necessary in my case, could leave in case stopImmediateProp isn't available? 
-    event.stopImmediatePropagation();
-    return false;
+body.oncontextmenu = function (event) {
+	event.preventDefault();
+	event.stopPropagation(); // not necessary in my case, could leave in case stopImmediateProp isn't available? 
+	event.stopImmediatePropagation();
+	return false;
 };
 
 
-if(localStorage.username != undefined) {
+if (localStorage.username != undefined) {
 	form.style.display = 'none'
 	game.style.display = 'flex'
 } else {
@@ -83,37 +83,36 @@ if(localStorage.username != undefined) {
 	game.style.display = 'none'
 }
 
-login.addEventListener('click', ()=>{
+login.addEventListener('click', () => {
 	const username = document.querySelector('#username').value
-	
-	if(username.length >= 3 && username.length <= 15) {
+
+	if (username.length >= 3 && username.length <= 15) {
 		form.style.display = 'none'
 		game.style.display = 'flex'
-	
+
 
 		localStorage.setItem('username', username)
 		saveUser(username)
 	} else {
 		const inscr = document.querySelector('.inscr')
-		console.log(inscr.style.fontSize);
 
 		inscr.style.color = 'red'
 		inscr.style.fontSize = (Number(inscr.style.fontSize.slice(0, -2)) + 1) + 'px'
 	}
-	
+
 })
 
 function gameStart() {
-	
-	reset()
 	isGame = true
+	reset()
+
 
 	gameLoop = setInterval(gameTick, 5)
 
 	huiSpawn = setInterval(() => {
 		setTimeout(() => {
 			huiCreate()
-		}, getRndInteger(0, 4000+huiSpeed*10*huisCount))
+		}, getRndInteger(0, 4000 + convSpeed * 10 * huisCount))
 	}, 2000)
 
 	scoreCounter = setInterval(() => {
@@ -130,9 +129,7 @@ function gameTick() {
 }
 
 
-function getPosition(element) {
-	return Number(element.style.left.slice(0, -2))
-}
+
 
 function convCreate(startPos) {
 	const conv = document.createElement('img')
@@ -147,7 +144,7 @@ function convCreate(startPos) {
 
 function moveConvs() {
 	convs.forEach(conv => {
-		conv.style.left = getPosition(conv) - huiSpeed + 'px'
+		conv.style.left = getPosition(conv) - convSpeed + 'px'
 		if (getPosition(conv) <= -conv.width) {
 			conv.style.left = conv.width + 'px'
 		}
@@ -177,7 +174,7 @@ function huiCreate() {
 function moveHuis() {
 	let huisToRemove = []
 	huis.forEach(hui => {
-		hui.style.left = getPosition(hui) - huiSpeed + 'px'
+		hui.style.left = getPosition(hui) - convSpeed + 'px'
 
 		if (getPosition(hui) <= -100) {
 			hui.remove()
@@ -185,7 +182,7 @@ function moveHuis() {
 			huisCount = huisCount + 1
 			huisCountOnScreen.textContent = huisCount
 			if (huisCount % 10 === 0 && huisCount >= -1) {
-				huiSpeed += 0.2
+				convSpeed += 0.2
 			}
 		}
 		if (collision(door, hui)) {
@@ -206,7 +203,7 @@ function reset() {
 	huis = []
 	score = 0
 	huisCount = 0
-	huiSpeed = 1
+	convSpeed = 1
 	scoreCountOnScreen.textContent = 0
 	huisCountOnScreen.textContent = 0
 	document.querySelectorAll('.hui').forEach(e => e.remove())
@@ -228,89 +225,48 @@ buttonRestart.addEventListener('click', () => {
 	gameStart()
 })
 
-
+function getPosition(element) {
+	return Number(element.style.left.slice(0, -2))
+}
 
 function collision(door, hui) {
 	let doorPosition = getPosition(door)
 	let huiPosition = getPosition(hui)
 
-	return doorPosition + 30 >= huiPosition
-		&& doorPosition + 10 <= huiPosition
-		&& Number(door.style.bottom.slice(0, -2)) <= Number(hui.style.height.slice(0, -2))
+	return doorPosition + 43 >= huiPosition
+		&& doorPosition <= huiPosition
+		&& Number(door.style.bottom.slice(0, -2)) <= 30
+		|| doorPosition + 13 >= huiPosition
+		&& doorPosition <= huiPosition
+		&& Number(door.style.bottom.slice(0, -2)) > 30
+		&& Number(door.style.bottom.slice(0, -2)) < 90
+		|| doorPosition >= huiPosition
+		&& doorPosition - 60 <= huiPosition
+		&& Number(door.style.bottom.slice(0, -2)) < 90
+		
 }
-
-
-function doorControl() {
-	let stamina = 200
-	let lossStamina
-	let increasingStamina
-
-	function handleDownEvent () {
-		if (Number(door.style.bottom.slice(0, -2)) === 0) {
-			if (stamina >= 200) {
-				door.style.transition = '0.2s'
-				door.style.bottom = '120px'
-			}
-			clearInterval(increasingStamina)
-			lossStamina = setInterval(() => {
-				stamina -= 1
-				staminaInd.style.width = stamina + 'px'
-				if (stamina <= 0) {
-					door.style.bottom = '1px'
-					setTimeout(() => {
-						door.style.bottom = '0px'
-					}, 200);
-					clearInterval(lossStamina)	
-				}
-			}, 10)
-		}
-	}
-	function handleUpEvent(){
-		clearInterval(increasingStamina)
-		door.style.bottom = '1px'
-		setTimeout(() => {
-			door.style.bottom = '0px'
-		}, 200);
-		clearInterval(lossStamina)
-		increasingStamina = setInterval(() => {
-			if (stamina < 200) {
-				stamina += 1
-				staminaInd.style.width = stamina + 'px'
-			}
-			if (stamina === 200) {
-				clearInterval(increasingStamina)
-			}
-		}, 5)
-	}
-
-	body.addEventListener('mousedown', handleDownEvent)
-	body.addEventListener('touchstart', handleDownEvent)
-	
-	body.addEventListener('mouseup', handleUpEvent)
-	body.addEventListener('touchend', handleUpEvent)
-}
-
 
 function gameEnd() {
+	isGame = false
 	clearInterval(gameLoop)
 	endScreen.style.display = 'flex'
 	scoreBoard.style.display = 'none'
 	gameField.style.marginTop = '33.33px'
 	buttonRestart.style.display = 'flex'
-	isGame = false
+
 	endScreen.innerHTML = `Отакої,<br>ви зачепилися за пеніс<br>Ваш рахуок: ${score}<br>Зібрано пенісів: ${huisCount}`
 	clearInterval(huiSpawn)
 	clearInterval(scoreCounter)
 	document.querySelectorAll('.hui').forEach(e => e.remove())
 	door.style.display = 'none'
 
-	if (Number(localStorage.score) <= score ) {
+	if (Number(localStorage.score) <= score) {
 		localStorage.setItem('score', score)
 		localStorage.setItem('huisCount', huisCount)
 		scoreRecord.textContent = score
 		huisRecord.textContent = huisCount
 		saveScore(localStorage.username, localStorage.huisCount, localStorage.score)
-	} 
+	}
 	if (!Number(localStorage.score)) {
 		localStorage.setItem('score', score)
 		localStorage.setItem('huisCount', huisCount)
@@ -318,10 +274,77 @@ function gameEnd() {
 		huisRecord.textContent = localStorage.huisCount
 		saveScore(localStorage.username, localStorage.huisCount, localStorage.score)
 	}
-
 }
 
 // disable userselect for iphone
-body.ontouchstart = function(e) {
+body.ontouchstart = function (e) {
 	e.preventDefault();
 }
+
+
+let stamina = 200;
+let lossStamina;
+let increasingStamina;
+let doorMove;
+
+function doorOpening() {
+	clearInterval(doorMove);
+	doorMove = setInterval(() => {
+		door.style.bottom = Number(door.style.bottom.slice(0, -2)) + 1 * convSpeed + 'px';
+		if (Number(door.style.bottom.slice(0, -2)) >= 120) { clearInterval(doorMove); }
+	}, 3);
+}
+
+function doorEnding() {
+	clearInterval(doorMove);
+	doorMove = setInterval(() => {
+		door.style.bottom = Number(door.style.bottom.slice(0, -2)) - 1*convSpeed + 'px';
+		if (Number(door.style.bottom.slice(0, -2)) <= 0) { clearInterval(doorMove); }
+	}, 3);
+}
+
+function handleDownEvent() {
+	if (Number(door.style.bottom.slice(0, -2)) <= 0) {
+		if (stamina >= 75) {
+			doorOpening();
+		}
+		clearInterval(increasingStamina);
+		lossStamina = setInterval(() => {
+			stamina -= 1 * convSpeed;
+			staminaInd.style.width = stamina + 'px';
+			if (stamina <= 0) {
+				doorEnding();
+				clearInterval(lossStamina);
+			}
+		}, 13);
+	}
+}
+
+function handleUpEvent() {
+	if (Number(door.style.bottom.slice(0, -2)) > 0) { doorEnding(); }
+	clearInterval(increasingStamina);
+	clearInterval(lossStamina);
+	increasingStamina = setInterval(() => {
+		if (stamina < 200) {
+			stamina += 1;
+			staminaInd.style.width = stamina + 'px';
+		}
+		if (stamina === 200) {
+			clearInterval(increasingStamina);
+		}
+	}, 10);
+}
+
+function doorControl() {
+	body.removeEventListener('mousedown', handleDownEvent);
+	body.removeEventListener('mouseup', handleUpEvent);
+	body.removeEventListener('touchstart', handleDownEvent);
+	body.removeEventListener('touchend', handleUpEvent);
+	
+	body.addEventListener('mousedown', handleDownEvent);
+	body.addEventListener('touchstart', handleDownEvent);
+
+	body.addEventListener('mouseup', handleUpEvent);
+	body.addEventListener('touchend', handleUpEvent);
+}
+
